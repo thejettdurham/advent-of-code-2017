2a82893ffd7ef6403843edb5c5bab8eeb95b06bf
Jett Durham <jett@jettdurham.com>
2017-12-07 23:06:50 -0600

    complete day 7

--- solutions/day7.js
+++ solutions/day7.js
@@ -7,7 +7,7 @@
 */
 
 const R = require("ramda");
-const logMe = R.tap(console.log);
+
 const readline = require("readline");
 const r1 = readline.createInterface({
   input: process.stdin,
@@ -57,12 +57,83 @@ const associateParents = nodes =>
 const buildDoubleLinkedTree = R.pipe(parseInput, associateParents);
 const findRootNode = R.find(R.pipe(R.prop("parent"), R.isNil));
 
-const part1 = R.pipe(buildDoubleLinkedTree, R.values, findRootNode, logMe, R.prop("name"));
-const part2 = () => "none";
+const getChildren = tree => node =>
+  R.pipe(R.prop("children"), R.map(R.prop(R.__, tree)))(node);
+
+const getBranchWeight = tree => node =>
+  R.ifElse(
+    R.propEq("children", null),
+    R.always([]),
+    R.pipe(getChildren(tree), children =>
+      R.pipe(
+        R.pluck("weight"),
+        w => [w, R.map(getBranchWeight(tree), children)],
+        R.flatten,
+        R.sum
+      )(children)
+    )
+  )(node);
+
+const getPgmWithDifferentWeight = pgms =>
+  R.pipe(
+    R.countBy(R.prop("branchWeight")),
+    R.invert,
+    R.prop(1),
+    R.ifElse(
+      R.isNil,
+      R.always(null),
+      R.pipe(R.head, Number, val => R.find(R.propEq("branchWeight", val))(pgms))
+    )
+  )(pgms);
+
+const getCorrectedWeight = pgms => R.pipe(
+  R.countBy(R.prop("branchWeight")),
+  R.invert,
+  R.values,
+  R.flatten,
+  ([wrong, right]) => right - wrong,
+  delta => R.pipe(
+    getPgmWithDifferentWeight,
+    R.prop("weight"),
+    R.add(delta),
+  )(pgms),
+)(pgms);
+
+const getBranchesWeights = (tree, previousPgms = {}) => node =>
+  R.pipe(
+    getChildren(tree),
+    R.map(child => ({
+      name: child.name,
+      weight: child.weight,
+      branchWeight: child.weight + getBranchWeight(tree)(child)
+    })),
+    pgms =>
+      R.pipe(
+        getPgmWithDifferentWeight,
+        R.ifElse(
+          R.isNil,
+          () => getCorrectedWeight(previousPgms),
+          R.pipe(
+            R.prop("name"),
+            R.prop(R.__, tree),
+            getBranchesWeights(tree, pgms)
+          )
+        )
+      )(pgms)
+  )(node);
+
+// returns a tuple of answers: [part1, part2]
+const parts = R.pipe(buildDoubleLinkedTree, indexedTree =>
+  R.pipe(R.values, findRootNode, rootNode => [
+    R.prop("name", rootNode),
+    getBranchesWeights(indexedTree)(rootNode)
+  ])(indexedTree)
+);
 
 const run = input => {
-  const part1ans = part1(input);
-  const part2ans = part2(input);
+  const answers = parts(input);
+  const part1ans = answers[0];
+  const part2ans = answers[1];
 
   console.log(
     `Given your input, Part 1 is ${part1ans} and Part 2 is ${part2ans}`
